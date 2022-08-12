719c9dacc95886e3a825211ebcccff7d5248e6ea
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-08-12 20:28:36 +0530

    commit_day3_2.0

---
+++ codeforces/problem_200-B/problem_200-B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define max(a, b) (a < b ? b : a)
+// #define min(a, b) ((a > b) ? b : a)
+// #define mod 1e9 + 7
+ #define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+// #define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
+// #define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+// #define INF 1000000000000000003
+// typedef long long int ll;
+// typedef vector<int> vi;
+// typedef pair<int, int> pi;
+// #define F first
+// #define S second
+// #define PB push_back
+// #define POB pop_back
+// #define MP make_pair
+int main()
+{
+ ios::sync_with_stdio(0);
+ cin.tie(0);
+ int T;
+ cin >> T;
+ int drinks[T];
+ FOR(i,T)
+ cin>>drinks[i];
+
+ double percentage;
+ FOR(i,T)
+ percentage+=drinks[i];
+
+ percentage=percentage;
+ percentage=percentage/T;
+    std::cout << std::fixed;
+std::cout << std::setprecision(2);
+ cout<<setprecision(8)<<percentage;
+ return 0;
+}
