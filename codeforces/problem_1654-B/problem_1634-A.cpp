332133cf8745b3f786aa7f1987001066cdea5567
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-08-15 20:37:37 +0530

    commit_day6

--- codeforces/problem_1654-B/problem_1634-A.cpp
+++ codeforces/problem_1654-B/problem_1634-A.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // #define max(a, b) (a < b ? b : a)
 // #define min(a, b) ((a > b) ? b : a)
 // #define mod 1e9 + 7
-#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+ #define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
 // #define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
 // #define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
 // #define INF 1000000000000000003
@@ -15,10 +15,38 @@ using namespace std;
 // #define PB push_back
 // #define POB pop_back
 // #define MP make_pair
+string rev(string su){
+    string s1;
+    for(int i=su.length()-1;i>=0;i--){
+        s1+=su[i];
+    }
+    return s1;
+}
 int main()
 {
  ios::sync_with_stdio(0);
  cin.tie(0);
- 
+ int T;
+ cin >> T;
+  int K,N,n=0;
+ string s;
+    int ans[1000000];
+ while (T--) {
+ cin >>N>>K>>s;
+    FOR (i,K)
+    {
+        s+=rev(s);
+    }
+     unordered_set<char> sh;
+   FOR(i,s.length()){
+    sh.insert(s[i]);
+   }
+   ans[n]=(int)sh.size();
+   n++;
+ }
+ FOR(i,n-1){
+     cout<<ans[i]<<endl; 
+ }
+  
  return 0;
 }
--- codeforces/problem_1703_A/problem_1703_A.cpp
+++ codeforces/problem_1703_A/problem_1703_A.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 // #define max(a, b) (a < b ? b : a)
 // #define min(a, b) ((a > b) ? b : a)
 // #define mod 1e9 + 7
-#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+ #define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
 // #define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
 // #define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
 // #define INF 1000000000000000003
@@ -19,6 +20,21 @@ int main()
 {
  ios::sync_with_stdio(0);
  cin.tie(0);
- 
+ int T;
+ cin >> T;
+  string s[T];
+
+ FOR(i,T) {
+ cin >> s[i];
+ transform(s[i].begin(),s[i].end(),s[i].begin(), ::toupper);
+ if(s[i]=="YES"){
+    s[i]="YES";
+ }
+ else{
+   s[i] ="NO";
+ }
+ }
+ FOR(i,T)
+ cout<<s[i]<<endl;
  return 0;
 }
