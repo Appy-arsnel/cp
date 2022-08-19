5ef16793debd264a55e9333ad2d266482956b5b1
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-08-19 23:32:02 +0530

    commit_day8_2.0

--- codeforces/problem_1154-A/tempCodeRunnerFile.cpp
+++ codeforces/problem_1154-A/tempCodeRunnerFile.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // #define max(a, b) (a < b ? b : a)
 // #define min(a, b) ((a > b) ? b : a)
 // #define mod 1e9 + 7
-// #define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+ #define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
 // #define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
 // #define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
 // #define INF 1000000000000000003
@@ -19,11 +19,29 @@ int main()
 {
  ios::sync_with_stdio(0);
  cin.tie(0);
- ll a,b,c,sum;
- cin>>b>>sum>>c>>a;
- a=sum-c;
- b=sum-b;
- c=sum-a-b;
- cout<<a<<" "<<b<<" "<<a;
+ int arr[4],arr_new[3];
+ int a,b,c,sum,max;
+ FOR(i,4)
+ cin>>arr[i];
+
+ max=arr[0];
+
+ for(int i=1;i<4;i++){
+    if(max<arr[i]){
+        max=arr[i];
+        sum=i;
+    }
+ }
+ int j=0;
+FOR(i,4)
+if(i!=sum){
+    arr_new[j]=arr[i];
+    j++;
+}
+
+ c=max-arr_new[0];
+ a=max-arr_new[2];
+ b=max-c-a;
+ cout<<a<<" "<<b<<" "<<c;
  return 0;
 }
